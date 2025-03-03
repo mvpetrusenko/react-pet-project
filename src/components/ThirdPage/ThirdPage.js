ddeeacec2c3969807f89524687f4029e3df8678d
mvpetrusenko <mvpetrusenko300@gmail.com>
2025-03-03 21:59:16 +0200

    search block design

--- src/components/ThirdPage/ThirdPage.js
+++ src/components/ThirdPage/ThirdPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../ThirdPage/ThirdPage.css' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 // import Header from '../Header/Header'; 
 // import NavBar from '../NavBar/NavBar';
 // import Footer from '../Footer/Footer'; 
@@ -11,6 +13,21 @@ function ThirdPage() {
         {<NavBar />} */}
         <div className="content"> 
             <p>Third Page Content</p>
+        </div> 
+
+
+        <div className='searchBlock'>
+          <input type='text' 
+          autoComplete='off' 
+          placeholder='Search...'
+          className='searchInput'></input>
+
+          <button className='buttonSearch'><FontAwesomeIcon icon={faMagnifyingGlass} className="magnifyingGlassIcon"/></button>
+        </div> 
+
+
+        <div className='searchResult'>
+            <p></p>
         </div>
         {/* {<Footer />} */}
       
