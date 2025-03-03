608c7f91b2faa3744755c0a0eddbbd90a0e5ccf0
mvpetrusenko <mvpetrusenko300@gmail.com>
2025-03-03 22:20:47 +0200

    search data

--- src/components/ThirdPage/ThirdPage.js
+++ src/components/ThirdPage/ThirdPage.js
@@ -27,7 +27,36 @@ function ThirdPage() {
 
 
         <div className='searchResult'>
-            <p></p>
+            <p style={{color: "red", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Red</p> 
+            <p style={{color: "green", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Green</p> 
+            <p style={{color: "yellow", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Yellow</p> 
+            <p style={{color: "orange", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Orange</p> 
+            <p style={{color: "blue", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Blue</p> 
+            <p style={{color: "purple", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Purple</p>
+            <p style={{color: "pink", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Pink</p> 
+            <p style={{color: "white", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>White</p> 
+            <p style={{color: "lightGray", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Gray</p>      
+            <p style={{color: "black", fontStyle: "italic", fontWeight: "bold", 
+              fontSize: "30px", borderStyle: "double", 
+              border: "3px solid white"}}>Black</p>             
         </div>
         {/* {<Footer />} */}
       
