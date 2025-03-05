39f905eee84421f0e9383bac39f71a0e1bedcc00
mvpetrusenko <mvpetrusenko300@gmail.com>
2025-03-05 20:26:32 +0200

    edited filter block design

--- src/components/FourthPage/FilterData.js
+++ src/components/FourthPage/FilterData.js
@@ -1,66 +1,69 @@
+const FilterData = [
+    {
+        id: "1", 
+        name: "laptop", 
+        category: "technology", 
+        color: "gray", 
+        price: 50
+    }, 
+    {
+        id: "2", 
+        name: "chair", 
+        category: "furniture", 
+        color: "brown", 
+        price: 100
+    }, 
+    {
+        id: "3", 
+        name: "phone", 
+        category: "technology", 
+        color: "red", 
+        price: 150
+    }, 
+    {
+        id: "4", 
+        name: "powerbank", 
+        category: "technology", 
+        color: "black", 
+        price: 200
+    }, 
+    {
+        id: "5", 
+        name: "jacket", 
+        category: "clothes", 
+        color: "black", 
+        price: 250
+    }, 
+    {
+        id: "6", 
+        name: "boots", 
+        category: "clothes", 
+        color: "brown", 
+        price: 300
+    }, 
+    {
+        id: "7", 
+        name: "shirt", 
+        category: "clothes", 
+        color: "brown", 
+        price: 350
+    }, 
+    {
+        id: "8", 
+        name: "jeans", 
+        category: "clothes", 
+        color: "blue", 
+        price: 400
+    }, 
+    {
+        id: "9", 
+        name: "banana", 
+        category: "food", 
+        color: "yellow", 
+        price: 450
+    }
+]
 
 
-
-
-
-// const FilterData = [
-//     {
-//         id: "1", 
-//         name: "laptop", 
-//         category: "technology", 
-//         color: "gray"
-//     }, 
-//     {
-//         id: "2", 
-//         name: "chair", 
-//         category: "furniture", 
-//         color: "brown"
-//     }, 
-//     {
-//         id: "3", 
-//         name: "phone", 
-//         category: "technology", 
-//         color: "red"
-//     }, 
-//     {
-//         id: "4", 
-//         name: "powerbank", 
-//         category: "technology", 
-//         color: "black"
-//     }, 
-//     {
-//         id: "5", 
-//         name: "jacket", 
-//         category: "clothes", 
-//         color: "black"
-//     }, 
-//     {
-//         id: "6", 
-//         name: "boots", 
-//         category: "clothes", 
-//         color: "brown"
-//     }, 
-//     {
-//         id: "7", 
-//         name: "shirt", 
-//         category: "clothes", 
-//         color: "brown"
-//     }, 
-//     {
-//         id: "8", 
-//         name: "jeans", 
-//         category: "clothes", 
-//         color: "blue"
-//     }, 
-//     {
-//         id: "9", 
-//         name: "banana", 
-//         category: "food", 
-//         color: "yellow"
-//     }
-// ] 
-
-
-
-// // export to import and use this code
-// export default FilterData; 
+// export to import and use this code
+export default FilterData; 
--- src/components/FourthPage/FourthPage.js
+++ src/components/FourthPage/FourthPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../FourthPage/FourthPage.css' 
+import FilterData from '../FourthPage/FilterData'; 
 
 // search for: react jsx render object (array) 
 // [] - array, {} - object 
@@ -16,7 +17,8 @@ import '../FourthPage/FourthPage.css'
   // </li>); 
 
 
-// css ul (not li !!!!) remove bullets (dots)
+// css ul (not li !!!!) remove bullets (dots) 
+// how to set name for a checkbox in react jsx
 
 
 
@@ -24,71 +26,71 @@ import '../FourthPage/FourthPage.css'
 function FourthPage() { 
 
 
-  const FilterData = [
-    {
-        id: "1", 
-        name: "laptop", 
-        category: "technology", 
-        color: "gray", 
-        price: 50
-    }, 
-    {
-        id: "2", 
-        name: "chair", 
-        category: "furniture", 
-        color: "brown", 
-        price: 100
-    }, 
-    {
-        id: "3", 
-        name: "phone", 
-        category: "technology", 
-        color: "red", 
-        price: 150
-    }, 
-    {
-        id: "4", 
-        name: "powerbank", 
-        category: "technology", 
-        color: "black", 
-        price: 200
-    }, 
-    {
-        id: "5", 
-        name: "jacket", 
-        category: "clothes", 
-        color: "black", 
-        price: 250
-    }, 
-    {
-        id: "6", 
-        name: "boots", 
-        category: "clothes", 
-        color: "brown", 
-        price: 300
-    }, 
-    {
-        id: "7", 
-        name: "shirt", 
-        category: "clothes", 
-        color: "brown", 
-        price: 350
-    }, 
-    {
-        id: "8", 
-        name: "jeans", 
-        category: "clothes", 
-        color: "blue", 
-        price: 400
-    }, 
-    {
-        id: "9", 
-        name: "banana", 
-        category: "food", 
-        color: "yellow", 
-        price: 450
-    }
-]
+//   const FilterData = [
+//     {
+//         id: "1", 
+//         name: "laptop", 
+//         category: "technology", 
+//         color: "gray", 
+//         price: 50
+//     }, 
+//     {
+//         id: "2", 
+//         name: "chair", 
+//         category: "furniture", 
+//         color: "brown", 
+//         price: 100
+//     }, 
+//     {
+//         id: "3", 
+//         name: "phone", 
+//         category: "technology", 
+//         color: "red", 
+//         price: 150
+//     }, 
+//     {
+//         id: "4", 
+//         name: "powerbank", 
+//         category: "technology", 
+//         color: "black", 
+//         price: 200
+//     }, 
+//     {
+//         id: "5", 
+//         name: "jacket", 
+//         category: "clothes", 
+//         color: "black", 
+//         price: 250
+//     }, 
+//     {
+//         id: "6", 
+//         name: "boots", 
+//         category: "clothes", 
+//         color: "brown", 
+//         price: 300
+//     }, 
+//     {
+//         id: "7", 
+//         name: "shirt", 
+//         category: "clothes", 
+//         color: "brown", 
+//         price: 350
+//     }, 
+//     {
+//         id: "8", 
+//         name: "jeans", 
+//         category: "clothes", 
+//         color: "blue", 
+//         price: 400
+//     }, 
+//     {
+//         id: "9", 
+//         name: "banana", 
+//         category: "food", 
+//         color: "yellow", 
+//         price: 450
+//     }
+// ]
 
 
 
@@ -105,7 +107,20 @@ const filterDataCards = FilterData.map(card => <li>
       <p style={{backgroundColor: "lightPink"}}>{card.color}</p>
       <p style={{backgroundColor: "purple", color: "white"}}>{card.price}</p>
     </div>
-</li>);
+</li>); 
+
+
+
+
+
+const handleFilterName = () => {
+    
+}
+
+
+
+
+
 
 
   return ( 
@@ -118,9 +133,67 @@ const filterDataCards = FilterData.map(card => <li>
 
             <div className='filter'>
                 <p>Filter</p> 
-                <button className='filterName'>name</button> 
-                <button className='filterCategory'>category</button> 
-                <button className='filterColor'>color</button>
+                <div className='filterCategory'>
+                    <p>Category</p> 
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='technology'></input>
+                      <label htmlFor='technology'>Technology</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='furniture'></input>
+                      <label htmlFor='furniture'>Furniture</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='clothes'></input>
+                      <label htmlFor='clothes'>Clothes</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='food'></input>
+                      <label htmlFor='food'>Food</label>
+                    </div>
+                    
+                </div>
+
+
+                
+                <div className='filterColor'>
+                    <p>Color</p> 
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='gray'></input>
+                      <label htmlFor='gray'>Gray</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='brown'></input>
+                      <label htmlFor='brown'>Brown</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='red'></input>
+                      <label htmlFor='red'>Red</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='black'></input>
+                      <label htmlFor='black'>Black</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='blue'></input>
+                      <label htmlFor='blue'>Blue</label>
+                    </div>
+
+                    <div className='checkboxGroup'>
+                      <input type='checkbox' name='yellow'></input>
+                      <label htmlFor='yellow'>Yellow</label>
+                    </div>
+
+                    
+                </div>
             </div>
           
 
